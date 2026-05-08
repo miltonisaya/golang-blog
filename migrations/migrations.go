065a274c29ce2dea87e6cd95c694ee395d2b8be7
Milton Isaya <miltonisaiah@gmail.com>
2026-05-08 15:49:47 +0300

    fixed package and created migration for post

--- migrations/migrations.go
+++ migrations/migrations.go
@@ -1,1 +1,20 @@
-package migrations
+package main
+
+import (
+	"blog/inits"
+	"blog/models"
+	"log"
+)
+
+func init() {
+	inits.LoadEnv()
+	inits.DBInit()
+}
+
+func main() {
+	err := inits.DB.AutoMigrate(&models.Post{})
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+}
