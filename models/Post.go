4a6c67163f3ce8972e3a3c738554e0a403630852
Milton Isaya <miltonisaiah@gmail.com>
2026-05-08 15:48:28 +0300

    created Post model

---
+++ models/Post.go
@@ -0,0 +1,12 @@
+package models
+
+import "gorm.io/gorm"
+
+type Post struct {
+	gorm.Model
+	Title  string `json:"title"`
+	Body   string `json:"body"`
+	Likes  int    `json:"likes"`
+	Draft  bool   `json:"draft"`
+	Author string `json:"author"`
+}
