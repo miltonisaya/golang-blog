ec0de65134e6f98dc158fc56004975a3e9ac36a8
Milton Isaya <miltonisaiah@gmail.com>
2026-05-08 16:29:54 +0300

    fix main: move DBInit to init(), register all CRUD routes for /posts, and read server port from PORT env variable

--- main.go
+++ main.go
@@ -1,23 +1,26 @@
 package main
 
 import (
+	"blog/controllers"
 	"blog/inits"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func init() {
 	inits.LoadEnv()
+	inits.DBInit()
 }
 
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello World",
-		})
-	})
 
-	inits.DBInit()
-	r.Run()
+	r.POST("/posts", controllers.CreatePost)
+	r.GET("/posts", controllers.GetPosts)
+	r.GET("/posts/:id", controllers.GetPost)
+	r.PUT("/posts/:id", controllers.UpdatePost)
+	r.DELETE("/posts/:id", controllers.DeletePost)
+
+	r.Run(":" + os.Getenv("PORT"))
 }
