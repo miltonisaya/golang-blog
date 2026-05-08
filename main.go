865b3d81afb55ca1babf75562d26e5a9a4ca93a0
Milton Isaya <miltonisaiah@gmail.com>
2026-05-08 15:13:41 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"blog/inits"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	inits.LoadEnv()
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
+
+	r.Run()
+}
